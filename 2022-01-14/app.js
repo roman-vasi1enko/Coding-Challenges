3a9e06de7d2ac0b4fad71d3daedf01228b28ce83
Roman Vasilenko <roman@vasilenko.co>
2022-01-14 11:42:10 -0500

    calculate bottles to cover holiday

---
+++ 2022-01-14/app.js
@@ -0,0 +1,18 @@
+// Task 1 - Duty Free
+  // The purpose of this task is to work out just how many bottles of duty free whiskey you would have to buy such that the saving over the normal high street price would effectively cover the cost of your holiday.
+
+  // You will be given the high street price (normPrice), the duty free discount (discount) and the cost of the holiday.
+
+  // For example, if a bottle cost £10 normally and the discount in duty free was 10%, you would save £1 per bottle. If your holiday cost £500, the answer you should return would be 500.
+
+  // All inputs will be integers. Please return an integer. Round down.
+
+    // Solution
+    function dutyFree(normPrice, discount, hol){
+      return Math.floor(hol / (normPrice * (discount / 100)));
+    }
+
+    // Tests
+    dutyFree(12, 50, 1000) // 166
+    dutyFree(17, 10, 500) // 294
+    dutyFree(24, 35, 3000) // 357

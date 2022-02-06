eb0423e5dcc9253978a715f92fb5441924903356
Roman Vasilenko <roman@vasilenko.co>
2022-02-06 13:00:24 -0500

    debug dunction syntax

---
+++ 2022-02-06/app.js
@@ -0,0 +1,16 @@
+// Task 1 - Function syntax debugging
+  // A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+  // function main [verb, noun]
+  //   return verb + noun
+  // }
+
+  // Solution
+  function main(verb, noun) {
+    return verb + noun;
+  }
+
+  // Tests
+  main();
+  main('take ', 'item') // 'take item'
+  main('use ', 'sword') // 'use sword'

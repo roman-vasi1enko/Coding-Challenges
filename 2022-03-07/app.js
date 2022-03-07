923e95a04e7283efe09e39fd350a166931777b6e
Roman Vasilenko <roman@vasilenko.co>
2022-03-07 11:54:57 -0500

    check if Leo gets oscar

---
+++ 2022-03-07/app.js
@@ -0,0 +1,24 @@
+// Task 1 - Leonardo Dicaprio and Oscars
+  // You have to write a function that describe Leo:
+
+  // function leo(oscar) {
+
+  // }
+  // if oscar was (integer) 88, you have to return "Leo finally won the oscar! Leo is happy".
+  // if oscar was 86, you have to return "Not even for Wolf of wallstreet?!"
+  // if it was not 88 or 86 (and below 88) you should return "When will you give Leo an Oscar?"
+  // if it was over 88 you should return "Leo got one already!"
+
+    // Solution
+    function leo(oscar){
+      return oscar == 88 ? 'Leo finally won the oscar! Leo is happy' 
+           : oscar == 86 ? 'Not even for Wolf of wallstreet?!' 
+           : oscar < 88 ? 'When will you give Leo an Oscar?' 
+           : 'Leo got one already!';
+    }
+
+    // Tests
+    leo(89) // "Leo got one already!
+    leo(88) // "Leo finally won the oscar! Leo is happy"
+    leo(87) // "When will you give Leo an Oscar?
+    leo(86) // "Not even for Wolf of wallstreet?!

70067691cd484852f99ffd55df300f0da4f7efa9
Roman Vasilenko <roman@vasilenko.co>
2022-04-11 15:12:16 -0400

    solve 2 coding challenges: strings, array methods

--- 2022-04-11/app.js
+++ 2022-04-11/app.js
@@ -13,3 +13,21 @@
         // Tests
         feast("great blue heron", "garlic naan") // true
         feast("brown bear", "bear claw") // false
+
+// Task 2
+    //altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+    //altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+    //Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+    // "hello world".toAlternatingCase() === "HELLO WORLD"
+    // "HELLO WORLD".toAlternatingCase() === "hello world"
+    // "hello WORLD".toAlternatingCase() === "HELLO world"
+    // "HeLLo WoRLD".toAlternatingCase() === "hEllO wOrld"
+
+          // Solution
+          String.prototype.toAlternatingCase = function () {
+            return this.split('').map(letter => letter === letter.toUpperCase() ? letter.toLowerCase() : letter.toUpperCase()).join('');
+          }
+
+          // Tests
+          'hello world'.toAlternatingCase() // 'HELLO WORLD'
+          'HeLLo WoRLD'.toAlternatingCase() // 'hEllO wOrld'

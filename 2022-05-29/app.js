9e67b2decb51e552443ecfb2e7e920732059bf40
Roman Vasilenko <roman@vasilenko.co>
2022-05-29 15:22:36 -0400

    Combine objects in one: JS coding challenge

---
+++ 2022-05-29/app.js
@@ -0,0 +1,33 @@
+// Task 1 - Combine objects
+  // Your task is to write a function that takes two or more objects and returns a new object which combines all the input objects.
+  // All input object properties will have only numeric values. Objects are combined together so that the values of matching keys are added together.
+  // An example:
+  const obj1 = { a: 10, b: 20, c: 30 }
+  const obj2 = { a: 3, c: 6, d: 3 }
+  combine(objA, objB) // Returns { a: 13, b: 20, c: 36, d: 3 }
+  // The combine function should be a good citizen, so should not mutate the input objects.
+
+    // Solution
+    function combine() {
+      let obj = {}
+    
+      for (let i = 0; i < arguments.length; i++) {
+            for (let key in arguments[i]) {
+              obj[key] = obj[key] ? obj[key] + arguments[i][key]: arguments[i][key];
+            }
+      }
+      return obj;
+    }
+
+    // Tests
+    const objA = { a: 10, b: 20, c: 30 }
+    const objB = { a: 3, c: 6, d: 3 }
+    const objC = { a: 5, d: 11, e: 8 }
+    const objD = { c: 3 }
+
+    combine(objA, objB) //{ a: 13, b: 20, c: 36, d: 3 }
+    combine(objA, objC) //{ a: 15, b: 20, c: 30, d: 11, e: 8 }
+    combine(objA, objB, objC) //{ a: 18, b: 20, c: 36, d: 14, e: 8 }
+    combine(objA, objC, objD) //{ a: 15, b: 20, c: 33, d: 11, e: 8 }
+    combine({}, {}, {}) //{}
+    combine(objA, objC, {}) //{ a: 15, b: 20, c: 30, d: 11, e: 8 }

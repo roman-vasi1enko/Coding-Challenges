7892c97042498b0a88e87499e74b7031915c07f3
Roman Vasilenko <roman@vasilenko.co>
2022-07-24 14:19:33 -0400

    check if the list has your friend

---
+++ 2022-07-24/app.js
@@ -0,0 +1,22 @@
+// Task 1 - Friend or Foe?
+  // Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+  // If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+  // Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+  // i.e.
+
+  // friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+  // Note: keep the original order of the names in the output.
+
+    // Solution
+    function friend(friends){
+      return friends.filter(name => name.length === 4);
+    }
+
+    // Tests
+    friend(["Ryan", "Kieran", "Mark"]) // ["Ryan", "Mark"]
+    friend(["Ryan", "Jimmy", "123", "4", "Cool Man"]) // ["Ryan"]
+    friend(["Jimm", "Cari", "aret", "truehdnviegkwgvke", "sixtyiscooooool"]) // ["Jimm", "Cari", "aret"]
+    friend(["Love", "Your", "Face", "1"]) // ["Love", "Your", "Face"]

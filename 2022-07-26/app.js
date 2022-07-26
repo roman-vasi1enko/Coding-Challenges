ee96b0f9e68e5c54f0cfd2b16f84dbbde0500b76
Roman Vasilenko <roman@vasilenko.co>
2022-07-26 14:56:18 -0400

    check what string ends with

---
+++ 2022-07-26/app.js
@@ -0,0 +1,16 @@
+// Task 1 - String Ends With?
+  // Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+  // Examples:
+
+  // solution('abc', 'bc') // returns true
+  // solution('abc', 'd') // returns false
+
+    // Solution
+    function solution(str, ending){
+      return str.endsWith(ending);
+    }
+
+    // Tests
+    solution('abcde', 'cde') // true
+    solution('abcde', 'abc') // false

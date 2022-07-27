89d76aee10b7179b3301bcea1bce986a57649ded
Roman Vasilenko <roman@vasilenko.co>
2022-07-27 15:06:02 -0400

    combine strings, sort and remove dups

---
+++ 2022-07-27/app.js
@@ -0,0 +1,20 @@
+// Task 1 - Two to One
+  // Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+  // Examples:
+  // a = "xyaabbbccccdefww"
+  // b = "xxxxyyyyabklmopq"
+  // longest(a, b) -> "abcdefklmopqwxy"
+
+  // a = "abcdefghijklmnopqrstuvwxyz"
+  // longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+    // Solution
+    function longest(s1, s2) {
+      return [...new Set(s1+s2)].sort().join('')
+    }
+
+    // Tests
+    longest("aretheyhere", "yestheyarehere") // "aehrsty"
+    longest("loopingisfunbutdangerous", "lessdangerousthancoding") // "abcdefghilnoprstu"
+    longest("inmanylanguages", "theresapairoffunctions") // "acefghilmnoprstuy"

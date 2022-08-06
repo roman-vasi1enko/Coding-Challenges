aa27215a7c0dc997af6c73a742d264a932f4d085
Roman Vasilenko <roman@vasilenko.co>
2022-08-06 17:52:59 -0400

    find the stray number

---
+++ 2022-08-06/app.js
@@ -0,0 +1,18 @@
+// Task 1 - Find the stray number
+  // You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+  // Complete the method which accepts such an array, and returns that single different number.
+
+  // The input array will always be valid! (odd-length >= 3)
+
+  // Examples
+  // [1, 1, 2] ==> 2
+  // [17, 17, 3, 17, 17, 17, 17] ==> 3
+
+    // Solution
+    function stray(numbers) {
+      return numbers.find(num => numbers.indexOf(num) === numbers.lastIndexOf(num));
+    }
+
+    // Tests
+    stray([1, 1, 2]) // 2

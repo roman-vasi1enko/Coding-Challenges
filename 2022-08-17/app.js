d54337523fc30a840ad7f55bcf1d546cc419488d
Roman Vasilenko <roman@vasilenko.co>
2022-08-17 16:28:59 -0400

    calc the bumps

--- 2022-08-17/app.js
+++ 2022-08-17/app.js
@@ -1,15 +1,16 @@
-// Task - Small enough?
-  // You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+// Task - Bumps in the Road
+    // Your car is old, it breaks easily. The shock absorbers are gone and you think it can handle about 15 more bumps before it dies totally.
 
-  // You can assume all values in the array are numbers.
+    // Unfortunately for you, your drive is very bumpy! Given a string showing either flat road (_) or bumps (n). If you are able to reach home safely by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
 
-    // Solution
-    function smallEnough(a, limit){
-      return Math.max(...a) <= limit;
-    }
+        // Solution
+        function bump(x){
+            return x.split('n').length <= 15 ? 'Woohoo!' : 'Car Dead'
+        }
 
-    // Tests
-    smallEnough([66, 101], 200) // true
-    smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100) // false
-    smallEnough([101, 45, 75, 105, 99, 107], 107) // true
-    smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120) // true
+        // Tests
+        bump("n") // "Woohoo!"
+        bump("__nn_nnnn__n_n___n____nn__nnn") // "Woohoo!"
+        bump("nnn_n__n_n___nnnnn___n__nnn__") // "Woohoo!"
+        bump("_nnnnnnn_n__n______nn__nn_nnn") // "Car Dead"
+        bump("______n___n_") // "Woohoo!"

ea171c40f0f6c976ea3d092028382bdb2e7d994a
Roman Vasilenko <roman@vasilenko.co>
2022-08-19 17:05:58 -0400

    sort the array

---
+++ 2022-08-19/app.js
@@ -0,0 +1,10 @@
+// Task - Sort arrays
+  // Just a simple sorting usage. Create a function that   returns the elements of the input-array / list sorted in lexicographical order.
+
+    // Solution
+    let sortme = function( names ){
+      return names.sort();
+    }
+
+    // Test
+    sortme(['one', 'two', 'three' ]) // ["one", "three", "two"]

d9f8f727b08b6fa98b81f74df0179cb48b80ed8c
Roman Vasilenko <roman@vasilenko.co>
2022-08-20 17:08:47 -0400

    find the greatest common divisor

---
+++ 2022-08-20/app.js
@@ -0,0 +1,18 @@
+// Task - Greatest common divisor
+  // Find the greatest common divisor of two positive integers. The integers can be large, so you need to find a clever solution.
+
+  // The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
+
+    // Solution
+    function mygcd(x, y){
+      for ( let i = Math.max(x, y); i > 0; i-- ){
+        if ( x % i === 0 && y % i === 0){
+          return i;
+        }
+      }
+    }
+
+    // Tests
+    mygcd(30,12) // 6
+    mygcd(8,9) // 1
+    mygcd(1,1) // 1

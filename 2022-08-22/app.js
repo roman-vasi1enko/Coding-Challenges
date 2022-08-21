7db14a83b631ff6c27b25d87cf97adf154ddff69
Roman Vasilenko <roman@vasilenko.co>
2022-08-21 16:45:08 -0400

    repeat the digits

---
+++ 2022-08-22/app.js
@@ -0,0 +1,18 @@
+// Task - Digits explosion
+// Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+
+// Examples
+// explode("312")
+// should return : "333122"
+// explode("102269")
+// should return : "12222666666999999999"
+
+    // Solution
+    function explode(s) {
+      return s.split('').map(num => num.repeat(num)).join('')
+    }
+
+    // Tests
+    explode("312") // "333122"
+    explode("102269") // "12222666666999999999"
+    explode("0") // ""

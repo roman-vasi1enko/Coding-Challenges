83a468e83574a67f2e4675219c4f513f7f86c8d2
Roman Vasilenko <roman@vasilenko.co>
2022-09-20 13:02:50 -0400

    find Nth smallest number

---
+++ 2022-09-20/app.js
@@ -0,0 +1,25 @@
+// Task - Nth Smallest Element
+  // Given an array/list of integers, find the Nth smallest element in the array.
+
+  // Notes
+  // Array/list size is at least 3.
+  // Array/list's numbers could be a mixture of positives , negatives and zeros.
+  // Repetition in array/list's numbers could occur, so don't remove duplications.
+  // Input >> Output Examples
+  // arr=[3,1,2]            n=2    ==> return 2 
+  // arr=[15,20,7,10,4,3]   n=3    ==> return 7 
+  // arr=[2,169,13,-5,0,-1] n=4    ==> return 2 
+  // arr=[2,1,3,3,1,2],     n=3    ==> return 2 
+
+    // Soliution
+    function nthSmallest(arr, pos){
+      return arr.sort((a,b) => a - b)[pos - 1];
+    }
+
+    // Tests
+    nthSmallest(                 [3,1,2], 2) //  2
+    nthSmallest(        [15,20,7,10,4,3], 3) //  7
+    nthSmallest(          [-5,-1,-6,-18], 4) // -1
+    nthSmallest([-102,-16,-1,-2,-367,-9], 5) // -2
+    nthSmallest(      [2,169,13,-5,0,-1], 4) //  2
+    nthSmallest(           [2,1,3,3,1,2], 3) //  2

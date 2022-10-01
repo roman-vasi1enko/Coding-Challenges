2840e7f5c6c1c90b96b73211ed5a5dedbc0f7949
Roman Vasilenko <roman@vasilenko.co>
2022-10-01 16:17:23 -0400

    fix string case

---
+++ 2022-10-01/app.js
@@ -0,0 +1,24 @@
+// Task - Fix string case
+  // you will be given a string that may have mixed uppercase and lowercase letters and your task is to convert that string to either lowercase only or uppercase only based on:
+
+  // make as few changes as possible.
+  // if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
+  // For example:
+
+  // solve("coDe") = "code". Lowercase characters > uppercase. Change only the "D" to lowercase.
+  // solve("CODe") = "CODE". Uppercase characters > lowecase. Change only the "e" to uppercase.
+  // solve("coDE") = "code". Upper == lowercase. Change all to lowercase.
+
+    // Solution
+    function solve(s){
+      let countUpper = 0;
+      let countLower = 0;
+      s.split('').map(char => char === char.toLowerCase() ? countLower++ : countUpper++)
+      return countUpper > countLower ? s.toUpperCase() : s.toLowerCase();
+    }
+
+    // Tests
+    solve("code") // "code"
+    solve("CODe") // "CODE"
+    solve("COde") // "code"
+    solve("Code") // "code"

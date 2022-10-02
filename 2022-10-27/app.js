fc573ab678c31c4d58909e7b984a6c533d125228
Roman Vasilenko <roman@vasilenko.co>
2022-10-02 14:41:00 -0400

    operate through nums of different type

---
+++ 2022-10-27/app.js
@@ -0,0 +1,17 @@
+// Task 1 - Divide and Conquer
+  // Given a mixed array of number and string representations of integers, add up the non-string integers and subtract this from the total of the string integers.
+
+  // Return as a number.
+
+    // Solution
+    function divCon(x){
+      let stringInt = 0;
+      let numberInt = 0;
+      x.map(num => typeof num === 'string' ? stringInt += Number(num) : numberInt += num);
+      return numberInt - stringInt;
+    }
+
+    // Tests
+    divCon([9, 3, '7', '3']) // 2
+    divCon(['5', '0', 9, 3, 2, 1, '9', 6, 7]) // 14 
+    divCon(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']) // 13 

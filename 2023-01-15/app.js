b2139e6253976b52b0293e1a7e7a884aa589933e
Roman Vasilenko <roman@vasilenko.co>
2023-01-15 16:00:10 -0500

    Get Full Name

---
+++ 2023-01-15/app.js
@@ -0,0 +1,36 @@
+// Task - FIXME: Get Full Name
+  // The code provided is supposed return a person's Full Name given their first and last names.
+
+  // But it's not working properly.
+
+  // Notes
+  // The first and/or last names are never null, but may be empty.
+
+  // Task
+  // Fix the bug so we can all go home early.
+  // class Dinglemouse{
+
+  //   constructor( firstName, lastName ){
+  //   }
+    
+  //   getFullName(){
+  //     return firstName+" "+lastName
+  //   }
+    
+  // }
+
+    // Solution
+    class Dinglemouse{
+      constructor( firstName, lastName ){
+        this._firstName=firstName;
+        this._lastName=lastName;
+      }
+      
+      getFullName(){
+        return `${this._firstName} ${this._lastName}`.trim();
+      } 
+    }
+
+    // Test
+    console.log(new Dinglemouse("Clint", "Eastwood").getFullName(), "Clint Eastwood");
+    console.log(new Dinglemouse("Clint", "").getFullName(), "Clint");

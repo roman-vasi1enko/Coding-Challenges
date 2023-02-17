51381d46e70a8cc7737dba76350a47c3bd3b9cc1
Roman Vasilenko <roman@vasilenko.co>
2023-02-17 09:41:19 -0500

    How many are smaller than me?

---
+++ 2023-02-17/app.js
@@ -0,0 +1,19 @@
+// Task - How many are smaller than me?
+  // Write a function that given, an array arr, returns an array containing at each index i the amount of numbers that are smaller than arr[i] to the right.
+
+  // For example:
+  // * Input [5, 4, 3, 2, 1] => Output [4, 3, 2, 1, 0]
+  // * Input [1, 2, 0] => Output [1, 1, 0]
+
+    // Solution
+    function smaller(nums) {
+      return nums.map((n, i) => nums.slice(i).filter(m => m < n).length);
+    }
+
+    // Tests
+    console.log(smaller([5, 4, 3, 2, 1]), [4, 3, 2, 1, 0]);
+    console.log(smaller([1, 2, 3]), [0, 0, 0]);
+    console.log(smaller([1, 2, 0]), [1, 1, 0]);
+    console.log(smaller([1, 2, 1]), [0, 1, 0]);
+    console.log(smaller([1, 1, -1, 0, 0]), [3, 3, 0, 0, 0]);
+    console.log(smaller([5, 4, 7, 9, 2, 4, 4, 5, 6]), [4, 1, 5, 5, 0, 0, 0, 0, 0]);

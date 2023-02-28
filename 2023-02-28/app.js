2310d6c731c55e752df1513e47ec153e46574e1a
Roman Vasilenko <roman@vasilenko.co>
2023-02-28 09:36:01 -0500

    The old switcheroo

---
+++ 2023-02-28/app.js
@@ -0,0 +1,22 @@
+// Task - The old switcheroo
+  // Write a function
+  // vowel2index(str)
+  // that takes in a string and replaces all the vowels [a,e,i,o,u] with their respective positions within that string.
+  // E.g:
+
+  // vowel2index('this is my string') == 'th3s 6s my str15ng'
+  // vowel2index('Codewars is the best site in the world') == 'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld'
+  // vowel2index('') == ''
+  // Your function should be case insensitive to the vowels.
+
+    // Solution
+    function vowel2index(str) {
+      let vowels = 'aeiouAEIOU';
+      return str.split('').map((char, i) => vowels.includes(char) ? char = i+1 : char).join('')
+    }
+    
+    // Tests
+    console.log(vowel2index('this is my string'),'th3s 6s my str15ng');
+    console.log(vowel2index('Codewars is the best site in the world'),'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld');
+    console.log(vowel2index('Tomorrow is going to be raining'), 'T2m4rr7w 10s g1415ng t20 b23 r2627n29ng');
+    console.log(vowel2index(''), '');

8b3f6263c14dac1ac08070c92edf80434ab4a129
Roman Vasilenko <roman@vasilenko.co>
2023-03-02 09:42:20 -0500

    Nth power

---
+++ 2023-03-02/app.js
@@ -0,0 +1,16 @@
+// Task - Nth power rules them all!
+  // You are provided with an array of positive integers and an additional integer n (n > 1).
+  // Calculate the sum of each value in the array to the nth power. Then subtract the sum of the original array.
+
+  // Examples
+  // {1, 2, 3}, 3  -->  (1^3 + 2^3 + 3^3 ) - (1 + 2 + 3)  -->  36 - 6  -->  30
+  // {1, 2}, 5     -->  (1^5 + 2^5) - (1 + 2)             -->  33 - 3  -->  30
+
+    // Solution
+    function modifiedSum(arr, n) {
+      return arr.map(num => num ** n).reduce((sum, num) => sum + num, 0) - arr.reduce((sum, num) => sum + num, 0)
+    }
+
+    // Tests
+    console.log(modifiedSum([1, 2, 3], 3), 30);
+    console.log(modifiedSum([1, 2], 5), 30);

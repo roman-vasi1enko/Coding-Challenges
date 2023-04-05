1e1d6ef877f3ddc523dcc5fe27a1dcecca031664
Roman Vasilenko <roman@vasilenko.co>
2023-04-05 07:37:36 -0400

    PatternCraft - Visitor

---
+++ 2023-04-05/app.js
@@ -0,0 +1,42 @@
+// Task - PatternCraft - Visitor
+  // The Visitor Design Pattern can be used, for example, to determine how an attack deals a different amount of damage to a unit in the StarCraft game.
+
+  // The pattern consists of delegating the responsibility to a different class.
+
+  // When a unit takes damage it can tell the visitor what to do with itself.
+
+  // Your Task
+  // Complete the code so that when a Tank attacks a Marine it takes 21 damage and when a Tank attacks a Marauder it takes 32 damage.
+
+  // The Marine's initial health should be set to 100 and the Marauder's health should be set to 125.
+
+  // You have 3 classes:
+
+  // Marine: has a health property and accept(visitor) method
+  // Marauder: has a health property and accept(visitor) method
+  // TankBullet: the visitor class. Has visitLight(unit) and visitArmored(unit) methods
+
+    // Solution
+    class Marine {
+      constructor() { this.health = 100; }
+      accept(visitor) { visitor.visitLight(this); }
+    }
+    class Marauder {
+      constructor() { this.health = 125; }
+      accept(visitor) { visitor.visitArmored(this); }
+    }
+    class TankBullet {
+      visitLight(unit) { unit.health -= 21; }
+      visitArmored(unit) { unit.health -= 32; }
+    }
+
+    // Tests
+    let bullet = new TankBullet();
+    let light = new Marine();
+    light.accept(bullet);
+    console.log(light.health, 100 - 21);
+
+    bullet = new TankBullet();
+    let armored = new Marauder();
+    armored.accept(bullet);
+    console.log(armored.health, 125 - 32);

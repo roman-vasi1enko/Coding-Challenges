ae0eced0c30a9479f3d33233a354acbe0d72b494
Roman Vasilenko <roman@vasilenko.co>
2023-04-10 08:40:39 -0400

    Milk and Cookies for Santa

---
+++ 2023-04-10/app.js
@@ -0,0 +1,21 @@
+// Task - Milk and Cookies for Santa
+  // It's almost Christmas Eve, so we need to prepare some milk and cookies for Santa! Wait... when exactly do we need to do that?
+
+  // Time for Milk and Cookies
+  // Complete the function function that accepts a Date object, and returns true if it's Christmas Eve (December 24th), false otherwise.
+
+  // Keep in mind Javascript's Date month is 0 based!
+
+  // Examples
+  // timeForMilkAndCookies(new Date(2013, 11, 24))  // true
+  // timeForMilkAndCookies(new Date(2013, 0, 23))   // false
+  // timeForMilkAndCookies(new Date(3000, 11, 24))  // true
+
+    // Solution
+    let timeForMilkAndCookies = date => date.getMonth() === 11 && date.getDate() === 24;
+
+
+    // Tests
+    console.log(timeForMilkAndCookies(new Date(2013, 11, 24)), true)
+    console.log(timeForMilkAndCookies(new Date(2013, 0, 23)), false)
+    console.log(timeForMilkAndCookies(new Date(3000, 11, 24)), true)

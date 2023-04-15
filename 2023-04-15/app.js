e67c4a9260c488466c74556b50b75f66ed49c4b4
Roman Vasilenko <roman@vasilenko.co>
2023-04-15 09:01:24 -0400

    Fire on the boat

---
+++ 2023-04-15/app.js
@@ -0,0 +1,16 @@
+// Task - Fire on the boat
+  // Enjoying your holiday, you head out on a scuba diving trip!
+
+  // Disaster!! The boat has caught fire!!
+
+  // You will be provided a string that lists many boat related items. If any of these items are "Fire" you must spring into action. Change any instance of "Fire" into "~~". Then return the string.
+
+  // Solution
+  function fireFight(s){
+    return s.split(' ').map((w) => w === 'Fire' ? w = '~~' : w).join(' ');
+   }
+
+  // Tests
+  console.log(fireFight("Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast"), "Boat Rudder Mast Boat Hull Water ~~ Boat Deck Hull ~~ Propeller Deck ~~ Deck Boat Mast");
+  console.log(fireFight("Mast Deck Engine Water Fire"), "Mast Deck Engine Water ~~");
+  console.log(fireFight("Fire Deck Engine Sail Deck Fire Fire Fire Rudder Fire Boat Fire Fire Captain"), "~~ Deck Engine Sail Deck ~~ ~~ ~~ Rudder ~~ Boat ~~ ~~ Captain");

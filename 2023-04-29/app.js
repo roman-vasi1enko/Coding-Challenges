6c4c22a6e92cc602c30ef5a7d3de216a18a9b5cf
Roman Vasilenko <roman@vasilenko.co>
2023-04-29 09:41:32 -0400

    find a meeting room

---
+++ 2023-04-29/app.js
@@ -0,0 +1,16 @@
+// Task - Find a Meeting Room
+  // Your job at E-Corp is both boring and difficult. It isn't made any easier by the fact that everyone constantly wants to have a meeting with you, and that the meeting rooms are always taken!
+
+  // In this kata, you will be given an array. Each value represents a meeting room. Your job? Find the first empty one and return its index (N.B. There may be more than one empty room in some test cases).
+
+  // 'X' --> busy
+  // 'O' --> empty
+  // If all rooms are busy, return "None available!"
+
+    // Solution
+    let meeting = x => !x.includes('O') ? 'None available!' : x.indexOf('O');
+
+    // Tests
+    console.log(meeting(['X', 'O', 'X']), 1);
+    console.log(meeting(['O','X','X','X','X']), 0);
+    console.log(meeting(['X','X','X','X','X']), 'None available!');

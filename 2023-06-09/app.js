83c6ca5fb93d0cdd9731446b81c2f3d3dc17d377
Roman Vasilenko <roman@vasilenko.co>
2023-06-09 14:21:30 -0400

    Ninja vs Samurai: Strike

---
+++ 2023-06-09/app.js
@@ -0,0 +1,28 @@
+// Task - Ninja vs Samurai: Strike
+  // Something is wrong with our Warrior class. The strike method does not work correctly. The following shows an example of this code being used:
+
+  // var ninja = new Warrior('Ninja');
+  // var samurai = new Warrior('Samurai');
+
+  // samurai.strike(ninja, 3);
+  // // ninja.health should == 70
+  // Can you figure out what is wrong?
+
+  // let Warrior = function(name){
+  //   this.name = name;  
+  //   this.health = 100;
+  // }
+  
+  // Warrior.strike = function(enemy, swings){
+  //   enemy.health = Math.max(0, enemy.health - (swings * 10));   
+  // }
+
+    // Solution
+    let Warrior = function(name){
+      this.name = name;  
+      this.health = 100;
+    }
+    
+    Warrior.prototype.strike = function(enemy, swings){
+      enemy.health = Math.max(0, enemy.health - (swings * 10));   
+    }

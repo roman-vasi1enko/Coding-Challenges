d5eec4dd034e97e0bb1253406b66f01ebbfd53fb
Roman Vasilenko <roman@vasilenko.co>
2023-07-04 16:06:54 -0400

    Filter Coffee

---
+++ 2023-07-04/app.js
@@ -0,0 +1,19 @@
+// Task - Filter Coffee
+  // You love coffee and want to know what beans you can afford to buy it.
+
+  // The first argument to your search function will be a number which represents your budget.
+
+  // The second argument will be an array of coffee bean prices.
+
+  // Your 'search' function should return the stores that sell coffee within your budget.
+
+  // The search function should return a string of prices for the coffees beans you can afford. The prices in this string are to be sorted in ascending order.
+
+    // Solution
+    let search = (budget, prices) => String(prices.filter(v => v <= budget).sort((a, b) => a - b));
+
+
+    // Tests
+    console.log(search(3, [6, 1, 2, 9, 2]), "1,2,2")
+    console.log(search(14, [7, 3, 23, 9, 14, 20, 7]), "3,7,7,9,14")
+    console.log(search(0, [6, 1, 2, 9, 2]), "") 

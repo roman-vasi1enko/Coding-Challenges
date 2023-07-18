22ce0c331502e5de94b80a45c127bf225998bc88
Roman Vasilenko <roman@vasilenko.co>
2023-07-18 07:26:32 -0400

    last in list

--- 2023-07-18/app.js
+++ 2023-07-18/app.js
@@ -32,3 +32,20 @@
     // Tests
     console.log(potatoes(82, 127, 80), 114);
     console.log(potatoes(93, 129, 91), 100);
+
+
+// Task - last in list
+  // Write a function last that accepts a list and returns the last element in the list.
+
+  // If the list is empty:
+
+  // In languages that have a built-in option or result type (like OCaml or Haskell), return an empty option
+
+  // In languages that do not have an empty option, just return null
+
+    // Solution
+    const last = xs => xs.length !== 0 ? xs[xs.length-1] : null;
+
+    // Tests
+    console.log( last([1,2,3]), 3, "last([1,2,3]) == 3");
+    console.log( last([]), null, "last( [] ) == null");

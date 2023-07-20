2ac5b14609e72578c089d2dfcef493c0e0d7800f
Roman Vasilenko <roman@vasilenko.co>
2023-07-20 07:33:47 -0400

    Check three and two

--- 2023-07-20/app.js
+++ 2023-07-20/app.js
@@ -24,3 +24,28 @@
     console.log(solve("RYT'>s&gO-.CM9AKeH?,5317tWGpS<*x2ukXZD"),[15,8,6,9]);
     console.log(solve("$Cnl)Sr<7bBW-&qLHI!mY41ODe"), [10,7,3,6])
     console.log(solve("@mw>0=QD-iAx!rp9TaG?o&M%l$34L.nbft"), [7,13,4,10]);
+
+
+// Task - Check three and two
+  // Given an array with exactly 5 strings "a", "b" or "c" (chars in Java, characters in Fortran), check if the array contains three and two of the same values.
+
+  // Examples
+  // ["a", "a", "a", "b", "b"] ==> true  // 3x "a" and 2x "b"
+  // ["a", "b", "c", "b", "c"] ==> false // 1x "a", 2x "b" and 2x "c"
+  // ["a", "a", "a", "a", "a"] ==> false // 5x "a"
+
+      // Solution
+      function checkThreeAndTwo(arr) {
+        let result = [];
+        
+        result.push(arr.filter(num => num === 'a').length);
+        result.push(arr.filter(num => num === 'b').length);
+        result.push(arr.filter(num => num === 'c').length);
+        
+        return result.includes(3) && result.includes(2);
+      }
+
+      // Tests
+      console.log(checkThreeAndTwo(["a", "a", "a", "b", "b"]), true);
+      console.log(checkThreeAndTwo(["a", "c", "a", "c", "b"]), false);
+      console.log(checkThreeAndTwo(["a", "a", "a", "a", "a"]), false);
